e85bc0d0be1d8a3bf69134953e48c2ee6acfa357
Mohit Ahuja <mohahuja@microsoft.com>
2021-04-06 17:32:11 -0400

    Initial code for Datalake Gen 2 Paths data resource

---
+++ azurerm/internal/services/storage/storage_data_lake_gen2_path_data.go
@@ -0,0 +1,154 @@
+package storage
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/storage/validate"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+	"github.com/tombuildsstuff/giovanni/storage/2019-12-12/datalakestore/paths"
+	"github.com/tombuildsstuff/giovanni/storage/accesscontrol"
+)
+
+func dataSourceStorageDataLakeGen2Path() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceStorageAccountSasRead,
+
+		Timeouts: &schema.ResourceTimeout{
+			Read: schema.DefaultTimeout(5 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+
+			// "storage_account_id": {
+			// 	Type:         schema.TypeString,
+			// 	Required:     true,
+			// 	ValidateFunc: validate.StorageAccountID,
+			// },
+
+			// "filesystem_name": {
+			// 	Type:         schema.TypeString,
+			// 	Required:     true,
+			// 	ValidateFunc: validateStorageDataLakeGen2FileSystemName,
+			// },
+
+			// "path": {
+			// 	Type:     schema.TypeString,
+			// 	Required: true,
+			// },
+			"id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"resource": {
+				Type:         schema.TypeString,
+				Required:     true,
+				Computed:     true,
+				ValidateFunc: validation.StringInSlice([]string{"directory"}, false),
+			},
+
+			"owner": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.IsUUID,
+			},
+
+			"group": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.IsUUID,
+			},
+
+			"ace": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"scope": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringInSlice([]string{"default", "access"}, false),
+							Default:      "access",
+						},
+						"type": {
+							Type:         schema.TypeString,
+							Required:     true,
+							Computed:     true,
+							ValidateFunc: validation.StringInSlice([]string{"user", "group", "mask", "other"}, false),
+						},
+						"id": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							ValidateFunc: validation.IsUUID,
+						},
+						"permissions": {
+							Type:         schema.TypeString,
+							Required:     true,
+							Computed:     true,
+							ValidateFunc: validate.ADLSAccessControlPermissions,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataStorageDataLakeGen2PathRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Storage.ADLSGen2PathsClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := paths.ParseResourceID(d.Get("id").(string))
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.GetProperties(ctx, id.AccountName, id.FileSystemName, id.Path, paths.GetPropertiesActionGetStatus)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[INFO] Path %q does not exist in File System %q in Storage Account %q - removing from state...", id.Path, id.FileSystemName, id.AccountName)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error retrieving Path %q in File System %q in Storage Account %q: %+v", id.Path, id.FileSystemName, id.AccountName, err)
+	}
+
+	d.Set("path", id.Path)
+	d.Set("resource", resp.ResourceType)
+	d.Set("owner", resp.Owner)
+	d.Set("group", resp.Group)
+
+	// The above `getStatus` API request doesn't return the ACLs
+	// Have to make a `getAccessControl` request, but that doesn't return all fields either!
+	resp, err = client.GetProperties(ctx, id.AccountName, id.FileSystemName, id.Path, paths.GetPropertiesActionGetAccessControl)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[INFO] Path %q does not exist in File System %q in Storage Account %q - removing from state...", id.Path, id.FileSystemName, id.AccountName)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error retrieving ACLs for Path %q in File System %q in Storage Account %q: %+v", id.Path, id.FileSystemName, id.AccountName, err)
+	}
+
+	acl, err := accesscontrol.ParseACL(resp.ACL)
+	if err != nil {
+		return fmt.Errorf("Error parsing response ACL %q: %s", resp.ACL, err)
+	}
+	d.Set("ace", FlattenDataLakeGen2AceList(acl))
+
+	return nil
+}
